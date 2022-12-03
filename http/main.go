bf6687e279a9b21ead546b38226f1dde801f45f4
Romitou <romitou@protonmail.com>
2022-12-03 18:17:43 +0100

    :sparkles: Add HTTP endpoints

---
+++ http/main.go
@@ -0,0 +1,87 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/romitou/disneytables/api"
+	"github.com/romitou/disneytables/database"
+	"github.com/romitou/disneytables/database/models"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func Start() {
+	r := gin.Default()
+
+	r.Use(func(context *gin.Context) {
+		authorization := context.GetHeader("Authorization")
+		if authorization == "" {
+			context.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		if strings.TrimPrefix("Bearer ", authorization) != os.Getenv("API_KEY") {
+			context.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		context.Next()
+	})
+
+	r.GET("/restaurants", func(c *gin.Context) {
+		restaurants, err := database.Get().Restaurants()
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		c.JSON(http.StatusOK, restaurants)
+	})
+
+	r.POST("/restaurantAvailabilities", func(c *gin.Context) {
+		var search api.RestaurantAvailabilitySearch
+		err := c.ShouldBindBodyWith(&search, binding.JSON)
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		availabilities, err := api.RestaurantAvailabilities(search)
+		if err != nil {
+			return
+		}
+
+		c.JSON(http.StatusOK, availabilities)
+		return
+	})
+
+	r.POST("/bookAlerts", func(c *gin.Context) {
+		var alert models.BookAlert
+		err := c.ShouldBindBodyWith(&alert, binding.JSON)
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		err = database.Get().CreateBookAlert(alert)
+		if err != nil {
+			return
+		}
+
+		c.JSON(http.StatusOK, nil)
+		return
+	})
+
+	r.GET("/bookAlerts", func(c *gin.Context) {
+		bookAlerts, err := database.Get().PendingBookAlerts()
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		c.JSON(http.StatusOK, bookAlerts)
+	})
+
+	err := r.Run("0.0.0.0:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
